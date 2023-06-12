4036bb3a7e381b87f798e59a5ec187b622fac62f
可以癫 <rikmorrrr@gmail.com>
2023-06-12 10:54:24 +0800

    reflection

---
+++ Assets/Editor/CreatCubeMap.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+public class CreatCubeMap : ScriptableWizard
+{
+
+    public Transform renderFromPosition;
+    public Cubemap cubemap;
+
+    void OnWizardUpdate()
+    {
+        helpString = "Select transform to render from and cubemap to render into";
+        isValid = (renderFromPosition != null) && (cubemap != null);
+    }
+
+    void OnWizardCreate()
+    {
+        // create temporary camera for rendering
+        GameObject go = new GameObject("CubemapCamera");
+        go.AddComponent<Camera>();
+        // place it on the object
+        go.transform.position = renderFromPosition.position;
+        // render into cubemap		
+        go.GetComponent<Camera>().RenderToCubemap(cubemap);
+
+        // destroy temporary camera
+        DestroyImmediate(go);
+    }
+
+    [MenuItem("GameObject/Render into Cubemap")]
+    static void RenderCubemap()
+    {
+        ScriptableWizard.DisplayWizard<CreatCubeMap>(
+            "Render cubemap", "Render!");
+    }
+}
